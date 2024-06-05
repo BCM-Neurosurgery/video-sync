d34a228d1bef789d603744dd3893abde0af6e7c2
Hegelim <thefirstzyw@hotmail.com>
2024-06-05 10:01:13 -0500

    Add video class

--- pyvideosync/video.py
+++ pyvideosync/video.py
@@ -1,7 +1,56 @@
+import cv2
+from pyvideosync import utils
+
+
 class Video:
     """
-    Parse mp4 file
+    A class to parse mp4 files and extract video metadata.
+
+    Attributes:
+    ----------
+    video_path : str
+        The path to the mp4 video file.
+    capture : cv2.VideoCapture
+        The OpenCV VideoCapture object for the video file.
+    frame_count : int
+        The total number of frames in the video.
+    fps : float
+        The frames per second (fps) of the video.
+    length : float
+        The total length of the video in seconds.
+    frame_width : int
+        The width of the video frames.
+    frame_height : int
+        The height of the video frames.
     """
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, video_path: str) -> None:
+        self.video_path = video_path
+        self.capture = cv2.VideoCapture(video_path)
+
+        if not self.capture.isOpened():
+            raise ValueError(f"Error opening video file {video_path}")
+
+        self.frame_count = int(self.capture.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.fps = self.capture.get(cv2.CAP_PROP_FPS)
+        self.length = self.frame_count / self.fps
+        self.frame_width = int(self.capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.frame_height = int(self.capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    def get_frame_count(self) -> int:
+        return self.frame_count
+
+    def get_length(self) -> float:
+        return self.length
+
+    def get_length_readable(self) -> str:
+        return utils.frame2min(self.frame_count, self.fps)
+
+    def get_fps(self) -> float:
+        return self.fps
+
+    def get_frame_width(self) -> int:
+        return self.frame_width
+
+    def get_frame_height(self) -> int:
+        return self.frame_height
