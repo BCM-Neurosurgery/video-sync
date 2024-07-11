a55e280ae713dd1f7ce7259c3dd87792b52c3b2b
Hegelim <thefirstzyw@hotmail.com>
2024-07-11 16:14:27 -0500

    Add sub-menu to return to video processing

--- pyvideosync/main.py
+++ pyvideosync/main.py
@@ -17,7 +17,6 @@ from pyvideosync.data_pool import DataPool
 from pyvideosync import utils
 from moviepy.editor import VideoFileClip, AudioFileClip
 import yaml
-import argparse
 import sys
 import pandas as pd
 from pathlib import Path
@@ -130,7 +129,9 @@ def align_audio_video(video_path, audio_path, output_path):
     video_clip = VideoFileClip(video_path)
     audio_clip = AudioFileClip(audio_path)
     video_clip = video_clip.set_audio(audio_clip)
-    video_clip.write_videofile(output_path, codec="libx264", audio_codec="aac")
+    video_clip.write_videofile(
+        output_path, codec="libx264", audio_codec="aac", logger="bar"
+    )
 
 
 def prompt_user_for_video_file(cam_mp4_files):
@@ -155,7 +156,7 @@ def prompt_user_for_video_file(cam_mp4_files):
             elif 1 <= choice <= len(cam_mp4_files):
                 return cam_mp4_files[choice - 1]
             else:
-                print(f"Please enter a number between 1 and {len(cam_mp4_files)}.")
+                print(f"Please enter a number between 0 and {len(cam_mp4_files)}.")
         except ValueError:
             print("Invalid input. Please enter a number.")
 
@@ -221,6 +222,7 @@ def main():
         if choice == "1":
             while True:
                 clear_screen()
+                print("Please select YAML config file")
                 config_path = select_config_file()
                 if not config_path:
                     print(
@@ -252,171 +254,182 @@ def main():
                     print(
                         "Initial file integrity check failed, exiting to initial screen."
                     )
+                    time.sleep(2)
                     break
                 else:
                     print("Initial file integrity check passed!")
+                    time.sleep(2)
 
-                # prompt for video file(s) to be processed
-                cam_mp4_files = datapool.get_mp4_filelist(cam_serial)
-                video_to_process = prompt_user_for_video_file(cam_mp4_files)
-                if not video_to_process:
-                    break
-                video_output_dir = os.path.join(
-                    output_dir, extract_basename(video_to_process)
-                )
-                os.makedirs(video_output_dir, exist_ok=True)
-
-                # configure logging
-                logger = configure_logging(video_output_dir, timestamp)
-                logger.debug(f"Configuration:\n{yaml.dump(config)}")
-
-                # find associated nev, ns5, json file to that video
-                logger.info(f"You selected {video_to_process}")
-                logger.info("Scanning folders to find associated files...")
-                video_path = os.path.join(cam_recording_dir, video_to_process)
-                video = Video(video_path)
-                abs_start_frame, abs_end_frame = datapool.get_mp4_abs_frame_range(
-                    video_to_process, cam_serial
-                )
-                selected_video_df = video.get_video_stats_df(
-                    abs_start_frame, abs_end_frame
-                )
-                associated_files = datapool.find_associated_files(video_to_process)
-                associated_json_df = pd.DataFrame.from_records(associated_files["JSON"])
-                associated_nev_df = pd.DataFrame.from_records(associated_files["NEV"])
-                associated_ns5_df = pd.DataFrame.from_records(associated_files["NS5"])
-
-                logger.debug(f"Selected VIDEO:\n{selected_video_df}")
-                logger.debug(f"Associated JSON:\n{associated_json_df}")
-                logger.debug(f"Associated NEV:\n{associated_nev_df}")
-                logger.debug(f"Associated NS5:\n{associated_ns5_df}")
-
-                logger.info("Loading camera JSON file")
-                json_path = datapool.get_abs_json_path(video_to_process)
-                videojson = Videojson(json_path)
-                camera_df = videojson.get_camera_df(cam_serial)
-                logger.debug(f"camera json df:\n{camera_df}")
-                logger.debug(
-                    f"num of unique frame ids in camera json: {len(camera_df['frame_id'].unique())}"
-                )
-                plot_histogram(
-                    camera_df,
-                    "frame_id",
-                    os.path.join(
-                        video_output_dir, "camera_json_frame_id_diff_hist.png"
-                    ),
-                )
-
-                all_merged_dfs = []
-                for i, (nev_dict, ns5_dict) in enumerate(
-                    zip(associated_files["NEV"], associated_files["NS5"])
-                ):
-                    logger.info(f"Entering {i+1}th iteration...")
-                    logger.info(
-                        f"Processig {nev_dict['nev_rel_path']} and {ns5_dict['ns5_rel_path']}",
+                while True:
+                    clear_screen()
+                    cam_mp4_files = datapool.get_mp4_filelist(cam_serial)
+                    video_to_process = prompt_user_for_video_file(cam_mp4_files)
+                    if not video_to_process:
+                        break
+                    video_output_dir = os.path.join(
+                        output_dir, extract_basename(video_to_process)
                     )
-                    chunk_output_dir = os.path.join(video_output_dir, str(i))
-                    os.makedirs(chunk_output_dir, exist_ok=True)
+                    os.makedirs(video_output_dir, exist_ok=True)
 
-                    logger.info("Getting chunk serial data from NEV...")
-                    nev_path = datapool.get_abs_nev_path(nev_dict["nev_rel_path"])
-                    nev = Nev(nev_path)
-                    nev_chunk_serial_df = nev.get_chunk_serial_df()
-                    logger.debug(f"nev_chunk_serial_df:\n{nev_chunk_serial_df}")
-                    nev.plot_cam_exposure_all(
-                        os.path.join(chunk_output_dir, "cam_exposure_all.png"),
-                        0,
-                        200,
+                    # configure logging
+                    logger = configure_logging(video_output_dir, timestamp)
+                    logger.debug(f"Configuration:\n{yaml.dump(config)}")
+
+                    # find associated nev, ns5, json file to that video
+                    logger.info(f"You selected {video_to_process}")
+                    logger.info("Scanning folders to find associated files...")
+                    video_path = os.path.join(cam_recording_dir, video_to_process)
+                    video = Video(video_path)
+                    abs_start_frame, abs_end_frame = datapool.get_mp4_abs_frame_range(
+                        video_to_process, cam_serial
+                    )
+                    selected_video_df = video.get_video_stats_df(
+                        abs_start_frame, abs_end_frame
+                    )
+                    associated_files = datapool.find_associated_files(video_to_process)
+                    associated_json_df = pd.DataFrame.from_records(
+                        associated_files["JSON"]
+                    )
+                    associated_nev_df = pd.DataFrame.from_records(
+                        associated_files["NEV"]
+                    )
+                    associated_ns5_df = pd.DataFrame.from_records(
+                        associated_files["NS5"]
                     )
 
-                    logger.info(f"Getting {ns5_channel} in NS5...")
-                    ns5_path = datapool.get_abs_ns5_path(ns5_dict["ns5_rel_path"])
-                    ns5 = Nsx(ns5_path)
-                    ns5_channel_df = ns5.get_channel_df(ns5_channel)
-                    logger.debug(f"ns5_channel_df:\n{ns5_channel_df}")
-                    ns5.plot_channel_array(
-                        config["channel_name"],
-                        os.path.join(chunk_output_dir, f"ns5_{ns5_channel}.png"),
-                    )
+                    logger.debug(f"Selected VIDEO:\n{selected_video_df}")
+                    logger.debug(f"Associated JSON:\n{associated_json_df}")
+                    logger.debug(f"Associated NEV:\n{associated_nev_df}")
+                    logger.debug(f"Associated NS5:\n{associated_ns5_df}")
 
-                    logger.info("Merging NEV and Camera JSON...")
-                    chunk_serial_joined = nev_chunk_serial_df.merge(
+                    logger.info("Loading camera JSON file...")
+                    json_path = datapool.get_abs_json_path(video_to_process)
+                    videojson = Videojson(json_path)
+                    camera_df = videojson.get_camera_df(cam_serial)
+                    logger.debug(f"camera json df:\n{camera_df}")
+                    logger.debug(
+                        f"num of unique frame ids in camera json: {len(camera_df['frame_id'].unique())}"
+                    )
+                    plot_histogram(
                         camera_df,
-                        left_on="chunk_serial",
-                        right_on="chunk_serial_data",
-                        how="inner",
+                        "frame_id",
+                        os.path.join(
+                            video_output_dir, "camera_json_frame_id_diff_hist.png"
+                        ),
                     )
-                    logger.debug(f"chunk_serial_df_joined:\n{chunk_serial_joined}")
 
-                    logger.info("Merging JSON, NEV with NS5...")
-                    ns5_slice = ns5.get_channel_df_between_ts(
-                        ns5_channel_df,
-                        chunk_serial_joined.iloc[0]["TimeStamps"],
-                        chunk_serial_joined.iloc[-1]["TimeStamps"],
+                    all_merged_dfs = []
+                    for i, (nev_dict, ns5_dict) in enumerate(
+                        zip(associated_files["NEV"], associated_files["NS5"])
+                    ):
+                        logger.info(f"Entering {i+1}th iteration...")
+                        logger.info(
+                            f"Processig {nev_dict['nev_rel_path']} and {ns5_dict['ns5_rel_path']}...",
+                        )
+                        chunk_output_dir = os.path.join(video_output_dir, str(i))
+                        os.makedirs(chunk_output_dir, exist_ok=True)
+
+                        logger.info("Getting chunk serial data from NEV...")
+                        nev_path = datapool.get_abs_nev_path(nev_dict["nev_rel_path"])
+                        nev = Nev(nev_path)
+                        nev_chunk_serial_df = nev.get_chunk_serial_df()
+                        logger.debug(f"nev_chunk_serial_df:\n{nev_chunk_serial_df}")
+                        nev.plot_cam_exposure_all(
+                            os.path.join(chunk_output_dir, "cam_exposure_all.png"),
+                            0,
+                            200,
+                        )
+
+                        logger.info(f"Getting {ns5_channel} in NS5...")
+                        ns5_path = datapool.get_abs_ns5_path(ns5_dict["ns5_rel_path"])
+                        ns5 = Nsx(ns5_path)
+                        ns5_channel_df = ns5.get_channel_df(ns5_channel)
+                        logger.debug(f"ns5_channel_df:\n{ns5_channel_df}")
+                        ns5.plot_channel_array(
+                            config["channel_name"],
+                            os.path.join(chunk_output_dir, f"ns5_{ns5_channel}.png"),
+                        )
+
+                        logger.info("Merging NEV and Camera JSON...")
+                        chunk_serial_joined = nev_chunk_serial_df.merge(
+                            camera_df,
+                            left_on="chunk_serial",
+                            right_on="chunk_serial_data",
+                            how="inner",
+                        )
+                        logger.debug(f"chunk_serial_df_joined:\n{chunk_serial_joined}")
+
+                        logger.info("Merging JSON, NEV with NS5...")
+                        ns5_slice = ns5.get_channel_df_between_ts(
+                            ns5_channel_df,
+                            chunk_serial_joined.iloc[0]["TimeStamps"],
+                            chunk_serial_joined.iloc[-1]["TimeStamps"],
+                        )
+                        all_merged = ns5_slice.merge(
+                            chunk_serial_joined,
+                            left_on="TimeStamp",
+                            right_on="TimeStamps",
+                            how="left",
+                        )
+                        all_merged_dfs.append(all_merged)
+
+                    # concat all_merged_dfs
+                    all_merged_concat_df = pd.concat(all_merged_dfs, ignore_index=True)
+                    running_total_frame_id = (
+                        all_merged_concat_df["frame_id"].dropna().astype(int).to_numpy()
                     )
-                    all_merged = ns5_slice.merge(
-                        chunk_serial_joined,
-                        left_on="TimeStamp",
-                        right_on="TimeStamps",
-                        how="left",
+                    logger.debug(
+                        f"Length of total frame id:{len(running_total_frame_id)}"
                     )
-                    all_merged_dfs.append(all_merged)
+                    logger.debug(
+                        f"Length of all unique: {len(np.unique(running_total_frame_id))}"
+                    )
+                    logger.debug(f"All merged concat df:\n{all_merged_concat_df}")
 
-                # concat all_merged_dfs
-                all_merged_concat_df = pd.concat(all_merged_dfs, ignore_index=True)
-                running_total_frame_id = (
-                    all_merged_concat_df["frame_id"].dropna().astype(int).to_numpy()
-                )
-                logger.debug(f"Length of total frame id:{len(running_total_frame_id)}")
-                logger.debug(
-                    f"Length of all unique: {len(np.unique(running_total_frame_id))}"
-                )
-                logger.debug(f"All merged concat df:\n{all_merged_concat_df}")
+                    # make save paths
+                    output_video_path = os.path.join(
+                        video_output_dir,
+                        f"video_{cam_serial}_sliced_{timestamp}.mp4",
+                    )
+                    audio_output_path = os.path.join(
+                        video_output_dir,
+                        f"audio_{cam_serial}_sliced_{timestamp}.wav",
+                    )
+                    final_output_path = os.path.join(
+                        video_output_dir,
+                        f"final_{cam_serial}_aligned_{timestamp}.mp4",
+                    )
 
-                # make save paths
-                output_video_path = os.path.join(
-                    video_output_dir,
-                    f"video_{cam_serial}_sliced_{timestamp}.mp4",
-                )
-                audio_output_path = os.path.join(
-                    video_output_dir,
-                    f"audio_{cam_serial}_sliced_{timestamp}.wav",
-                )
-                final_output_path = os.path.join(
-                    video_output_dir,
-                    f"final_{cam_serial}_aligned_{timestamp}.mp4",
-                )
+                    logger.info("Processing valid audio...")
+                    valid_audio = utils.keep_valid_audio(all_merged_concat_df)
+                    utils.analog2audio(
+                        valid_audio, ns5.get_sample_resolution(), audio_output_path
+                    )
+                    logger.info(f"Saved sliced audio to {audio_output_path}")
 
-                logger.info("Processing valid audio...")
-                valid_audio = utils.keep_valid_audio(all_merged_concat_df)
-                utils.analog2audio(
-                    valid_audio, ns5.get_sample_resolution(), audio_output_path
-                )
-                logger.info(f"Saved sliced audio to {audio_output_path}")
+                    logger.info("Slicing video...")
+                    output_fps = len(running_total_frame_id) / (
+                        len(valid_audio) / ns5.get_sample_resolution()
+                    )
+                    logger.debug(f"Input video frame count: {video.get_frame_count()}")
+                    logger.debug(f"Input video fps: {video.get_fps()}")
+                    logger.debug(f"Output video fps: {output_fps}")
+                    offset_frame_id = running_total_frame_id - (abs_start_frame - 1)
+                    video.slice_video(output_video_path, offset_frame_id, output_fps)
+                    logger.info(f"Saved sliced video to {output_video_path}")
 
-                logger.info("Slicing video...")
-                output_fps = len(running_total_frame_id) / (
-                    len(valid_audio) / ns5.get_sample_resolution()
-                )
-                logger.debug(f"Input video frame count: {video.get_frame_count()}")
-                logger.debug(f"Input video fps: {video.get_fps()}")
-                logger.debug(f"Output video fps: {output_fps}")
-                offset_frame_id = running_total_frame_id - (abs_start_frame - 1)
-                video.slice_video(output_video_path, offset_frame_id, output_fps)
-                logger.info(f"Saved sliced video to {output_video_path}")
+                    logger.info("Aligning audio and video...")
+                    align_audio_video(
+                        output_video_path, audio_output_path, final_output_path
+                    )
+                    logger.info(f"Final video saved to {final_output_path}")
 
-                logger.info("Aligning audio and video...")
-                align_audio_video(
-                    output_video_path, audio_output_path, final_output_path
-                )
-                logger.info(f"Final video saved to {final_output_path}")
+                    remove_file(output_video_path)
+                    remove_file(audio_output_path)
+                    logger.info(f"Removed intermediate media files")
 
-                remove_file(output_video_path)
-                remove_file(audio_output_path)
-                logger.info(f"Removed intermediate media files")
-
-                logger.info("Video Sync complete!! Returning to video selection...")
-                time.sleep(3)
+                    logger.info("Video Sync complete!! Returning to video selection...")
+                    time.sleep(3)
 
         elif choice == "2":
             print("Exiting program. Goodbye!")
