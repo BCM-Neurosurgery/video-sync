f791887f29531f7f83ed3d644aa7e35bf91a51f6
Hegelim <thefirstzyw@hotmail.com>
2024-07-26 20:29:42 -0500

    Refactored to ui.py

--- pyvideosync/main.py
+++ pyvideosync/main.py
@@ -7,8 +7,6 @@ import os
 from pyvideosync.data_pool import DataPool
 import yaml
 import pandas as pd
-import tkinter as tk
-from tkinter import filedialog
 import time
 from pyvideosync.logging_config import (
     get_current_ts,
@@ -31,75 +29,12 @@ from pyvideosync.process import (
     save_frame_duration_to_file,
     combine_video_audio,
 )
-
-
-def prompt_user_for_video_file(cam_mp4_files):
-    """Prompt the user to select a video file from the list."""
-    if not cam_mp4_files:
-        print("No video files found.")
-        return None
-
-    print("Available video files:")
-    for idx, file in enumerate(cam_mp4_files):
-        print(f"{idx + 1}: {file}")
-
-    while True:
-        try:
-            choice = int(
-                input(
-                    "Enter the number of the file you want to process, or type 0 to go back to previous menu: "
-                )
-            )
-            if choice == 0:
-                return choice
-            elif 1 <= choice <= len(cam_mp4_files):
-                return cam_mp4_files[choice - 1]
-            else:
-                print(f"Please enter a number between 0 and {len(cam_mp4_files)}.")
-        except ValueError:
-            print("Invalid input. Please enter a number.")
-
-
-def select_config_file():
-    root = tk.Tk()
-    root.withdraw()
-    config_file_path = filedialog.askopenfilename(
-        title="Select Configuration File",
-        filetypes=(("YAML files", "*.yaml"), ("All files", "*.*")),
-    )
-    root.destroy()
-    return config_file_path
-
-
-def clear_screen():
-    if os.name == "nt":
-        os.system("cls")
-    else:
-        os.system("clear")
-
-
-def remove_file(file_path):
-    try:
-        os.remove(file_path)
-    except Exception as e:
-        print(f"Error removing file: {e}")
-
-
-def welcome_screen():
-    clear_screen()
-    print("\nWelcome to Video Sync Program v1.0")
-    print("Please select an option:")
-    print("1. Load a config file")
-    print("2. Exit")
-    choice = input("Enter your choice: ")
-    return choice
-
-
-def select_config():
-    clear_screen()
-    print("Please select YAML config file")
-    config_path = select_config_file()
-    return config_path
+from pyvideosync.ui import (
+    welcome_screen,
+    select_config,
+    clear_screen,
+    prompt_user_for_video_file,
+)
 
 
 def print_and_sleep(msg):
